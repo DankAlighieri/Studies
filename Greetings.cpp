9265d2a01c10e192509e7aec6427e0cfe477aad8
Laukim <54158328+DankAlighieri@users.noreply.github.com>
2022-08-20 19:27:50 -0300

    c++ studies

---
+++ Greetings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string name;
+    cout << "Whats your name?\n";
+    cin >> name;
+    cout << "Welcome " << name << "!";
+
+    return 0;
+};
---
+++ Hello_World.cpp
@@ -0,0 +1,6 @@
+#include <iostream>//input output stream
+
+int main() {
+    std::cout << "Hello World!\n"; //this will output to the console hello world
+    system("pause>0");
+}

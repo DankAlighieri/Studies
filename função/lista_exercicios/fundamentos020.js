ea7458545fcab561bfc3089952ea28cfd403428b
Laukim <guilhermeemeterioif@gmail.com>
2023-01-03 21:06:59 -0300

    new lines

--- função/lista_exercicios/fundamentos020.js
+++ função/lista_exercicios/fundamentos020.js
@@ -1,3 +1,16 @@
-function voltarValor(valor) {
-    
+function voltarValor(valorSaque) {
+    let contador100 = 0;
+    let contador50 = 0;
+    let contador10 = 0;
+    let contador5 = 0;
+    let contador1 = 0;
+    let valorNota = calcularValorNota(valorSaque)
+    while (valorSaque >= valorNota){
+        switch (valorNota) {
+            case 100:
+                valorSaque -= 100
+                contador100++
+                break
+        }
+    }
 }

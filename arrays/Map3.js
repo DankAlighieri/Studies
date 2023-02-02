9e6eaf5b1459b5dd95539854a4d26bd18ba7bbbc
Laukim <guilhermeemeterioif@gmail.com>
2023-02-02 20:02:15 -0300

    new notes

---
+++ arrays/Map3.js
@@ -0,0 +1,6 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }',
+  ];

eab495fed2bf7b5adc548f2b3aaf127058093686
Guilherme <guilhermeemeterioif@gmail.com>
2023-04-10 14:52:51 -0300

    Modifications on the gulp files

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -1,16 +1,18 @@
-const { series } = require('gulp') ;
+const { series, parallel } = require('gulp') ;
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const { countBy } = require('lodash');
 
-function padrao(cb) {
+function transformacaoJS(cb) {
     gulp.src('src/**/*.js')
         .pipe(babel({
             comments: false,
             presets: ['@babel/preset-env'] // Updated to import @babel/preset-env correctly
         }))
         .pipe(uglify())
+        .on('error', err => console.log(err))
         .pipe(concat('codigo.min.js'))
         .pipe(gulp.dest('build'))
         .on('end', cb);
@@ -18,4 +20,9 @@ function padrao(cb) {
     return;
 }
 
-exports.default = gulp.series(padrao);
+function fim (cb){
+    console.log('Fim!');
+    return cb();
+}
+
+exports.default = parallel(transformacaoJS, fim);

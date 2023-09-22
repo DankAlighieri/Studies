47e31d8c92cf247beef1623729018ef198720de4
Guilherme <guilhermeemeterioif@gmail.com>
2023-09-22 16:19:23 -0300

    Learning Angular Services by consuming the PokeApi and building a pokedex searcher.

---
+++ WebDev/DIO/Angular/C10 - Services e Pipes/services/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CardComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ WebDev/DIO/Angular/C10 - Services e Pipes/services/src/app/components/card/card.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  pokemon:PokemonData
+
+  constructor(
+    private service:PokemonService
+  ) {
+    this.pokemon = {
+      id:0,
+      name:'',
+      sprites:{front_default: ''},
+      types: []
+    };
+  }
+
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(searchName:string) {
+    searchName = searchName.toLocaleLowerCase();
+    this.service.getPokemon(searchName).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id:res.id,
+          name:res.name,
+          sprites:res.sprites,
+          types:res.types
+        }
+        console.log(this.pokemon);
+      },
+      error: (err) => console.log('not found')
+    });
+  }
+}
